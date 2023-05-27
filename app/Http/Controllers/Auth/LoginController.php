14cca91255bca69dec195112ce2fbd110e2406ca
hnrd <cg@zknt.org>
2023-05-27 11:27:56 +0200

    remove IP logging

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -108,8 +108,8 @@ class LoginController extends Controller
         $log->action = 'auth.login';
         $log->message = 'Account Login';
         $log->link = null;
-        $log->ip_address = $request->ip();
-        $log->user_agent = $request->userAgent();
+	$log->ip_address = "127.0.0.23";
+	$log->user_agent = "Pixelfed.de";
         $log->save();
     }
 
