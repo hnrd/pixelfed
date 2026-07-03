1e7c48c8516f435872a2b429c12c6e97907a8ef0
chris <cg@ritona.int.zknt.org>
2026-07-03 14:01:19 +0200

    Link legal notice
    local jurisdiction requires a prominent link to a legal notice at the frontpage

--- resources/assets/components/landing/sections/footer.vue
+++ resources/assets/components/landing/sections/footer.vue
@@ -7,9 +7,7 @@
             <div class="spacer">·</div>
             <a href="/site/privacy">Privacy</a>
             <div class="spacer">·</div>
-            <a v-if="config.show_legal_notice_link" href="/site/legal-notice">Legal Notice</a>
-            <div v-if="config.show_legal_notice_link" class="spacer">·</div>
-            <a href="https://pixelfed.org/mobile-apps" target="_blank">Mobile Apps</a>
+            <a href="/site/legal-notice">Impressum / Legal Notice</a>
         </div>
 
         <div class="footer-component-attribution">
