f872611bc42a46a0ba834d5b6af34f3d23132deb
chris <cg@ritona.int.zknt.org>
2026-09-17 11:39:18 +0200

    disable beagle
    beagle is a remote API service provided by dansup and used for centralised lookups.
    Using the beagle service without users explicit consent violates GDPR.
    As it's not configurable at the moment this patch disables remote communication of user data with beagle.

--- app/Services/Account/RemoteAuthService.php
+++ app/Services/Account/RemoteAuthService.php
@@ -147,6 +147,7 @@ class RemoteAuthService
 
     public static function isDomainCompatible($domain = false)
     {
+        return true;
         if (! $domain) {
             return false;
         }
@@ -180,6 +181,7 @@ class RemoteAuthService
 
     public static function lookupWebfingerUses($wf)
     {
+        return 0;
         try {
             $res = Http::timeout(20)->retry(3, 750)->get('https://beagle.pixelfed.net/api/v1/raa/lookup?webfinger='.$wf);
             if (! $res->ok()) {
@@ -202,6 +204,7 @@ class RemoteAuthService
 
     public static function submitToBeagle($ow, $ou, $dw, $du)
     {
+        return;
         try {
             $url = 'https://beagle.pixelfed.net/api/v1/raa/submit';
             $res = Http::throw()->timeout(10)->get($url, [
